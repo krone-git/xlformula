6dc6d42cafa2e15b847dc7568fbe7b21c8d1a4a7
BKrone <kr1.brandon@gmail.com>
2020-09-21 17:38:17 -0500

    Adding documentation

--- exception.py
+++ exception.py
@@ -6,4 +6,5 @@ Module contains all exceptions available for error handling.
 
 from .lib.exception import __all__ as __exception_all__
 
+# Set '*' imports to that of the 'exception' module;
 __all__ = __exception_all__
