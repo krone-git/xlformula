0098836bdd83dac4a31a6e1cfb59bf08e8abfbaf
bkrone <bkrone@nsekits.com>
2020-09-23 09:51:24 -0500

    Adding builtin constant variables

--- lib/builtin/__init__.py
+++ lib/builtin/__init__.py
@@ -6,7 +6,11 @@ from ._function import __all__ as __function_all__
 from ._formula import *     # Import all builtin formula classes;
 from ._formula import __all__  as __formula_all__
 
+from ._constant import *     # Import all builtin constant variables;
+from ._constant import __all__  as __constant_all__
+
 __all__ = (
     *__function_all__,
-    *__formula_all__
+    *__formula_all__,
+    *__constant_all__,
     )
